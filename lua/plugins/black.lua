9d3080b08c9486de9b4a4766994f70b5db1768e2
chuptani <yashchothani1324@gmail.com>
2025-12-18 21:00:02 +0100

    [2025-12-18]

---
+++ lua/plugins/black.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        -- Use the "black" command (which will now find your system version)
+        python = { "black" },
+      },
+    },
+  },
+}
