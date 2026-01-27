a39a4be9ff5cd681553bc9c24861c254d228d92d
chuptani <yashchothani1324@gmail.com>
2026-01-27 21:00:03 +0100

    [2026-01-27]

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,9 +5,7 @@ vim.opt.winbar = "%=%m %f"
 vim.g.snacks_animate = false
 
 -- vim.g.loaded_netrwPlugin = 1
-vim.g.netrw_liststyle = 3
-
--- Use Zathura as the PDF viewer
+vim.g.netrw_liststyle = 3 -- Use Zathura as the PDF viewer
 vim.g.vimtex_view_method = "zathura"
 
 -- Set the sync mode to 'focus' to sync with the existing viewer window
@@ -20,3 +18,5 @@ vim.g.vimtex_mappings_enabled = 0
 -- vim.g.copilot_assume_mapped = true
 -- vim.g.copilot_no_tab_map = true
 -- vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+-- vim.opt.suffixesadd:append(".md")
